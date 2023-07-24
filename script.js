5ce06e25602c8eae3e41d46a8ff91811ef99b43a
kagabojaphet <iradukundajaphet@gmail.com>
2023-07-24 10:08:53 +0200

    add

---
+++ script.js
@@ -0,0 +1,8 @@
+function reg(){
+ document.querySelector(".register-form").style.display="flex";
+ document.querySelector(".loginn-form").style.display="none";
+}
+function lg(){
+    document.querySelector(".register-form").style.display="none";
+    document.querySelector(".loginn-form").style.display="flex";
+   }
