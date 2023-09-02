b0b544da5b6df94b076178c4b9f3e3a243e2b842
kagabojaphet <iradukundajaphet@gmail.com>
2023-09-02 17:53:24 +0200

    add

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ window.onscroll = ()=>{
     menu.classList.remove('fa-times');
     header.classList.remove('active')
 }
+
+let popup = document.querySelector('#menu-popup');
+let forms = document.querySelector('.popup-container');
+
+popup.onclick = ()=>{
+    popup.classList.toggle('fa-times');
+    forms.classList.toggle('active')
+}
+
+
+window.onscroll = ()=>{
+    popup.classList.remove('fa-times');
+    forms.classList.remove('active')
+}
+
