4e31e0c062288d7b5d567416fd23059668ed1173
kagabojaphet <iradukundajaphet@gmail.com>
2023-09-20 13:44:44 +0200

    add

--- src/utils/errormessage.js
+++
@@ -1,7 +0,0 @@
-const errormessage=(res,status,message)=>{
-    res.status(status).json({
-        message:message
-    })
-}
-
-export default errormessage
--- src/utils/successmessage.js
+++
@@ -1,7 +0,0 @@
-const successmessage=(res,status,message,data)=>{
-    res.status(status).json({
-        message:message,
-        data:data
-    })
-}
-export default successmessage
