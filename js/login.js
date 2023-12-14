72734c10a5c8fe4e7be212f2bd49e25c037f13e9
kagabojaphet <iradukundajaphet59@gmail.com>
2023-12-14 11:22:25 +0200

    add

--- js/login.js
+++ js/login.js
@@ -1,41 +1,29 @@
-/*Store Data Using localStorage*/
-
-// let email=document.getElementById("email")
-// let password=document.getElementById("password")
-// let form=document.querySelector("form")
-
-// let fashoin=JSON.parse(localStorage.getItem("fashoin")) || []
-// form.addEventListener("submit",(e)=>{
-//     e.preventDefault()
-// const userExist=fashoin.find((user)=>user.email==email.value)
-// if(userExist && userExist.password==password.value){
-//     alert("Hello? Your Welcome")
-//     window.location.href="./home.html"
-// }else if(userExist && userExist.password!=password.value){
-// alert("Please Wrong Password !!!")
-// }else{
-//     alert("User Not Exist! Please Click Sign-up Now!!!")
-// }
-// })
-
-
-
-
-
-
-
 const LoginForm=document.querySelector(".login");
 
 LoginForm.addEventListener("submit",(e)=>{
-    e.preventDefault();
-
+    e.preventDefault()
     const email=document.querySelector("#email").value;
     const password=document.querySelector("#password").value;
 
     const data={
         email,
-        password
+        password,
     };
-    const api=``
     
+    const api=`https://long-gold-xerus-belt.cyclic.app/api/v1/user/login`
+    const setpost={
+        method:`POST`,
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify(data)
+    };
+    fetch(api,setpost)
+    .then((response)=>{
+        return response.json()
+    })
+    .then((data)=>{
+        console.log(data.message)
+    })
+
 })
